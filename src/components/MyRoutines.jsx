f0a068afbcc378a5385c4989b69f05f83290cc27
timDeHof <this.is.tim.dehof@gmail.com>
2022-04-13 15:50:45 -0400

    added goals to My routines

--- src/components/MyRoutines.jsx
+++ src/components/MyRoutines.jsx
@@ -1,7 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { createRoutines } from "../api/myRoutines";
 import useAuth from "../hooks/useAuth";
-
+/**
+ *   As a registered user on the My Routines tab, I want to:
+ *     [x] - be shown a form to create a new routine
+ *           -- the form should have text fields for name and goal
+ *         - for each routine which is owned by me I should
+ *              [] - be able to update the name and goal for the routine
+ *              [] - be able to delete the entire routine
+ *              [] - be able to add an activity to a routine via a small form which has
+ *                  a dropdown for all activities, an inputs for count and duration
+ *              [] - be able to update the duration or count of any activity
+ *                   on the routine
+ *              [] - be able to remove any activity from the routine
+ */
 const CreatingRoutines = () => {
   const { token, user } = useAuth();
   const [title, setTitle] = useState("");
--- src/components/Routines.jsx
+++ src/components/Routines.jsx
@@ -6,17 +6,6 @@ import { getRoutines } from "../api/routines";
  *         The routine name, goal, and creator's username
  *         A list of activities for the routine, including their name, description, and *         duration and/or count
  *
- *   As a registered user on the My Routines tab, I want to:
- *     [x] - be shown a form to create a new routine
- *           -- the form should have text fields for name and goal
- *         - for each routine which is owned by me I should
- *              [] - be able to update the name and goal for the routine
- *              [] - be able to delete the entire routine
- *              [] - be able to add an activity to a routine via a small form which has
- *                  a dropdown for all activities, an inputs for count and duration
- *              [] - be able to update the duration or count of any activity
- *                   on the routine
- *              [] - be able to remove any activity from the routine
  */
 function Routines({ userObj, token }) {
   const [routines, setRoutines] = useState([]);
