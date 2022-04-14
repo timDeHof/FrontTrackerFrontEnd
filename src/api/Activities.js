9babc41b64fc1fc0e4bbac2ddcf2ac48fb9b2b59
Aaron Reyes <ProdAronBeatz@gmail.com>
2022-04-14 10:02:50 -0500

    made a comment

--- src/api/Activities.js
+++ src/api/Activities.js
@@ -1,7 +1,7 @@
 export const BASE_URL = "https://fitnesstrac-kr.herokuapp.com";
 export const COHORT = "2202-FTB-ET-WEB-FT";
 const URL = BASE_URL + "/api";
-
+// making a comment for branch push
 export const getActivities = async () => {
   try {
     const response = await fetch(`${URL}/activities`);
