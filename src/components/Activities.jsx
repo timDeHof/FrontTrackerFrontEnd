8c107c12d3ee3b8c3bac572a9da73d3ed033f364
timDeHof <this.is.tim.dehof@gmail.com>
2022-04-14 14:57:56 -0400

    fixed error caused after merging with main

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -1,27 +1,20 @@
+import React, { useState, useEffect } from "react";
+import { getActivities } from "../api/Activities";
+import CreatingActivities from "./CreatingActivities";
+import useAuth from "../hooks/useAuth";
 
+function Activities() {
+  const { isLoggedIn } = useAuth();
+  const [activities, setActivities] = useState("");
 
-import React, { useState } from "react";
-/**
- * GOALS:
- *  As an unregistered visitor on the Activities tab, I want to:
- *     [ ] - see a list of all activities which have been created
- *
- *  As a registered user on the Activities tab, I want to:
- *     [ ] - be shown a form to create a new activity (by name and description)
- *     [ ] - be shown an error if the activity already exists
- *
- */
-function Activities(props) {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [activity, setActivity] = useState("");
-
-  /**
-   * Returns whether or not the user is authenticated.
-   * @returns {boolean} - Whether or not the user is authenticated.
-   */
-  const authenticated = localStorage.getItem("token") ? true : false;
-
+  useEffect(() => {
+    const getAllActivities = async () => {
+      const response = await getActivities();
+      console.log(response, "Awaiting the response or not");
+      setActivities(response);
+    };
+    getAllActivities();
+  }, []);
 
   return (
     <div className="activity">
