fa33418503bcd0689f7b5f8a5902297b7a08bba0
timDeHof <this.is.tim.dehof@gmail.com>
2022-04-14 22:34:41 -0400

    checklist updating

--- src/components/MyRoutines.jsx
+++ src/components/MyRoutines.jsx
@@ -4,11 +4,11 @@ import GetFilteredRoutines from "./GetFilteredRoutines";
 
 /**
  *   As a registered user on the My Routines tab, I want to:
- *     [x] - be shown a form to create a new routine
+ *     [WIP] - be shown a form to create a new routine
  *           -- the form should have text fields for name and goal
  *         - for each routine which is owned by me I should
  *              [] - be able to update the name and goal for the routine
- *              [] - be able to delete the entire routine
+ *              [x] - be able to delete the entire routine
  *              [] - be able to add an activity to a routine via a small form which has
  *                  a dropdown for all activities, an inputs for count and duration
  *              [] - be able to update the duration or count of any activity
