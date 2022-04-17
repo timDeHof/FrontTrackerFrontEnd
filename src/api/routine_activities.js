98b3f39c9ead044cb3cb994ed6a6b3d1bf0d39f4
timDeHof <this.is.tim.dehof@gmail.com>
2022-04-17 08:41:01 -0400

    created PATCH and DELETE ajax function

---
+++ src/api/routine_activities.js
@@ -0,0 +1,44 @@
+export const BASE_URL = "https://fitnesstrac-kr.herokuapp.com";
+const URL = BASE_URL + "/api";
+
+export const updateRoutineActivity = async (
+  { count, duration },
+  routineActivityId,
+  token
+) => {
+  try {
+    const response = await fetch(
+      `${URL}/routine_activities/${routineActivityId}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ count, duration }),
+      }
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("uh oh, trouble updating routine activity", error);
+  }
+};
+
+export const deleteRoutineActivity = async (routineActivityId, token) => {
+  try {
+    const response = await fetch(
+      `${URL}/routine_activities/${routineActivityId}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const data = await response.json();
+  } catch (error) {
+    console.error("uh oh, trouble deleting activity!", error);
+  }
+};
